60a3dce7367f364d1c1a13a5fa5f8beb2a076bb3
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-17 12:48:29 -0500

    User controller, model created

---
+++ db/migrate/20200117171626_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :encrypted_password
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name:'admin', email:'ptonyptc@gmail.com', encrypted_password:'admin123', status:1 )
+User.create(name:'anthony', email:'ptonyp19@hotmail.com', encrypted_password:'123456', status:2 )
