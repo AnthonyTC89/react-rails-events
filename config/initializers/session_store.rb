625a30ffa88fbb106db60d4ede3dbacb46284f9e
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-18 17:26:11 -0500

    gem rack-cors installed and updated some files

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_react-rails-events', domain: 'react-rails-events.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_react-rails-events' 
+end
