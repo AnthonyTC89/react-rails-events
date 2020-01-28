0af384fe8bb32eb608e4ab334dd2cae6051990bb
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-27 22:09:30 -0500

    rubocob issues solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae officiis temporibus libero nam hic! Libero autem quia eum beatae necessitatibus?'
 admin = User.new(username: 'admin', email: 'admin@gmail.com', password: 'admin', password_confirmation: 'admin', status: 1)
 admin.save
-superUser = User.new(username: 'superuser', email: 'superuser@gmail.com', password: 'superuser', password_confirmation: 'superuser', status: 2)
-superUser.save
+super_user = User.new(username: 'superuser', email: 'superuser@gmail.com', password: 'superuser', password_confirmation: 'superuser', status: 2)
+super_user.save
 User.create(username: 'user', email: 'user@gmail.com', password: 'user', password_confirmation: 'user', status: 3)
 Event.create(title: 'Event 1 Title', description: lorem, date: Date.today, time: Time.now, location: 'Arequipa', status: 1, user_id: admin.id)
 Event.create(title: 'Event 2 Title', description: lorem, date: Date.today - 1, time: Time.now, location: 'Puno', status: 1, user_id: admin.id)
-Event.create(title: 'Event 3 Title', description: lorem, date: Date.today + 1, time: Time.now, location: 'Lima', status: 1, user_id: superUser.id)
-Event.create(title: 'Event 4 Title', description: lorem, date: Date.today + 30, time: Time.now, location: 'Cusco', status: 1, user_id: superUser.id)
+Event.create(title: 'Event 3 Title', description: lorem, date: Date.today + 1, time: Time.now, location: 'Lima', status: 1, user_id: super_user.id)
+Event.create(title: 'Event 4 Title', description: lorem, date: Date.today + 30, time: Time.now, location: 'Cusco', status: 1, user_id: super_user.id)
