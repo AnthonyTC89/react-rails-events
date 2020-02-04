3f4915f6964bacc0ef077502a8d1b413afe58299
AnthonyTC89 <ptonyptc@gmail.com>
2020-02-04 14:32:25 -0500

    test for user controller done

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -8,33 +8,116 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'GET #index' do
     before do
       DatabaseCleaner.start
-      get :index
+      create(:user)
+      create(:admin)
+      create(:superuser)
     end
 
     after do
       DatabaseCleaner.clean
     end
 
-    it "returns http success" do
+    it "return expected all users" do
+      get :index
       expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(2)
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      DatabaseCleaner.start
     end
 
-    it 'return a valid unique users on creating' do
+    after do
+      DatabaseCleaner.clean
+    end
+
+    it "returns valid user" do
       user = create(:user)
-      expect(user.valid?).to be(true)
-      superuser = create(:superuser)
-      expect(superuser.valid?).to be(true)
-      admin = create(:admin)
-      expect(admin.valid?).to be(true)
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response['username']).to eql(user.username)
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      DatabaseCleaner.start
+      create(:admin)
+    end
+
+    after do
+      DatabaseCleaner.clean
+    end
+
+    it 'return a valid post create user' do
+      data = {
+        user: {
+          username: 'user', 
+          email: 'user@user.com', 
+          password: 'user', 
+          password_confirmation: 'user', 
+          status: 3
+        }
+      }
+      post :create , params: data
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      user_response = json_response['user']
+      expect(user_response['username']).to eql('user')
+    end
+    
+    it 'return a invalid user' do
+      data = {
+        user: {
+          username: 'admin', 
+          email: 'admin@admin.com', 
+          password: 'admin', 
+          password_confirmation: 'admin', 
+          status: 1
+        }
+      }
+      post :create , params: data
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'PATCH/PUT #update' do
+    before do
+      DatabaseCleaner.start
+    end
+
+    after do
+      DatabaseCleaner.clean
+    end
+
+    it 'return an accepted update user' do
+      user = create(:user)
+      put :update, params: { id: user.id, user: { username: 'updated' }}
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response['username']).to eql('updated')
+    end
+  end
+
+  describe 'DELETE #destroy' do 
+    before do
+      DatabaseCleaner.start
+    end
+
+    after do
+      DatabaseCleaner.clean
+    end
+    
+    it 'return an accepted update user' do
+      user = create(:user)
+      delete :destroy, params: {id: user.id}
+      expect(response).to have_http_status(:success)
       user = build(:user)
-      expect(user.valid?).to be(false)
+      expect(user.valid?).to be(true)
     end
   end
 end
-    # it "JSON body response contains expected users" do
-    #   admin = create(:user)
-    #   p "admin: #{admin}"
-    #   json_response = JSON.parse(response.body)
-    #   p "json_response: #{json_response}"
-    #   expect(json_response.keys).to match_array([:id, :username, :email])
-    # end
